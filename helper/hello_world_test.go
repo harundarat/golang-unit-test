688f9e8c00e4847784db551d77ab4b3891f3bb74
harundarat <harundarat@gmail.com>
2025-06-29 19:03:15 +0700

    t.Fail() t.FailNow() t.Error() t.Fatal()

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,11 +1,26 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Harun")
 
 	if result != "Hello Harun" {
-		panic("result not Hello Harun")
+		t.Error("result must be Hello Harun")
 	}
+
+	fmt.Println("TestHelloWorldHarun DONE")
+}
+
+func TestHelloWorldRasyid(t *testing.T) {
+	result := HelloWorld("Rasyid")
+
+	if result != "Hello Rasyid" {
+		t.Fatal("result must be Hello Rasyid")
+	}
+
+	fmt.Println("TestHelloWorldRasyid DONE")
 }
