7193c3e5b74ed4b7b696ff7df3249f9595a02fd1
harundarat <harundarat@gmail.com>
2025-06-30 08:46:47 +0700

    skip test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -2,24 +2,32 @@ package helper
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "linux" {
+		t.Skip("Cannot run on linux")
+	}
+
+	fmt.Println("runtime: " + runtime.GOOS)
+	result := HelloWorld("Harun")
+	assert.Equal(t, "Hello Harun", result, "Result must be 'Hello Harun'")
+	fmt.Println("This will be executed")
+}
+
 func TestHelloWorldAssert(t *testing.T) {
 	result := HelloWorld("Harun")
-
 	assert.Equal(t, "Hello Harun", result, "Result must be 'Hello Harun'")
-
 	fmt.Println("This will be executed")
 }
 
 func TestHElloWorldRequire(t *testing.T) {
 	result := HelloWorld("Harun")
-
 	require.Equal(t, "Hi Harun", result, "Result must be 'Hello Harun'")
-
 	fmt.Println("This won't be executed")
 }
